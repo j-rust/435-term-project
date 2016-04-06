beff39ae402ddcd765c69f1801356b3e56d5f891
jrust0707 <jrust@rams.colostate.edu>
2016-04-05 21:46:12 -0600

    Villanova championship config and minor tweak to output file path

--- src/main/java/com/vientiuno/term/project/App.java
+++ src/main/java/com/vientiuno/term/project/App.java
@@ -17,6 +17,9 @@ public class App
 {
     public static void main( String[] args ) throws Exception {
 
+        String outPath = args[1];
+        if(outPath.charAt(outPath.length()-1) != '/') outPath += "/";
+
         Configuration uncChampionshipConf = new Configuration();
         uncChampionshipConf.set("start_time", "Mon Apr 04 19:15:00 MDT 2016");
         uncChampionshipConf.set("end_time", "Mon Apr 04 21:30:00 MDT 2016");
@@ -36,9 +39,33 @@ public class App
         uncChampionship.setReducerClass(Reduce.class);
 
         FileInputFormat.setInputPaths(uncChampionship, new Path(args[0]));
-        FileOutputFormat.setOutputPath(uncChampionship, new Path(args[1]));
+        FileOutputFormat.setOutputPath(uncChampionship, new Path(outPath + "uncChampionship"));
 
         uncChampionship.waitForCompletion(true);
+
+        Configuration novaChampionshipConf = new Configuration();
+        novaChampionshipConf.set("start_time", "Mon Apr 04 19:15:00 MDT 2016");
+        novaChampionshipConf.set("end_time", "Mon Apr 04 21:30:00 MDT 2016");
+        novaChampionshipConf.set("keywords", "NovaMBB, Villanova, GoNova, NovaNation, #Nova, LetsMarchNova");
+
+        Job novaChampionship = Job.getInstance(novaChampionshipConf, "main");
+
+        novaChampionship.setJobName("Villanova Championship");
+
+        novaChampionship.setJarByClass(App.class);
+
+        novaChampionship.setOutputKeyClass(Text.class);
+        novaChampionship.setOutputValueClass(IntWritable.class);
+
+        novaChampionship.setMapperClass(Map.class);
+        novaChampionship.setCombinerClass(Reduce.class);
+        novaChampionship.setReducerClass(Reduce.class);
+
+        FileInputFormat.setInputPaths(novaChampionship, new Path(args[0]));
+        FileOutputFormat.setOutputPath(novaChampionship, new Path(outPath + "villanova"));
+
+        novaChampionship.waitForCompletion(true);
+
 
     }
 }
