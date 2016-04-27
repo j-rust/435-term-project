8e871256c2f1572d6365487ec0067db62489399a
jrust0707 <jrust@rams.colostate.edu>
2016-04-27 11:43:00 -0600

    Added popular words for each game and changed input paths to generate graphs

--- src/main/java/com/vientiuno/jchart/graphs/App.java
+++ src/main/java/com/vientiuno/jchart/graphs/App.java
@@ -12,17 +12,17 @@ public class App
     public static void main( String[] args ) throws Exception {
 
 
-        LineChart championship = new LineChart("Championship", "novaChampionship.txt", "Nova", "uncChampionship.txt", "UNC");
+        LineChart championship = new LineChart("Championship", "data/novaChampionship.txt", "Nova", "data/uncChampionship.txt", "UNC");
         championship.pack();
         RefineryUtilities.centerFrameOnScreen(championship);
         championship.setVisible(true);
 
-        LineChart ff1 = new LineChart("Nova-OU Final Four", "novaFF.txt", "Nova", "ouFF.txt", "OU");
+        LineChart ff1 = new LineChart("Nova-OU Final Four", "data/novaFF.txt", "Nova", "data/ouFF.txt", "OU");
         ff1.pack();
         RefineryUtilities.centerFrameOnScreen(ff1);
         ff1.setVisible(true);
 
-        LineChart ff2 = new LineChart("Cuse-UNC Final Four", "cuseFF.txt", "Syracuse", "uncFF.txt", "UNC");
+        LineChart ff2 = new LineChart("Cuse-UNC Final Four", "data/cuseFF.txt", "Syracuse", "data/uncFF.txt", "UNC");
         ff2.pack();
         RefineryUtilities.centerFrameOnScreen(ff2);
         ff2.setVisible(true);
